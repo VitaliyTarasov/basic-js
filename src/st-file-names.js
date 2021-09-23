e00397e016096ca7738e8313f456c08a97c26b73
viva <vivasaravian@gmail.com>
2021-09-23 20:00:57 +0300

    feat: complete st-file-names

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -15,7 +15,63 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+  const obj = {};
+  const d = [];
+  for (let i = 0; i < names.length; i++) {
+    if (names[i] in obj) {
+      obj[names[i]] += 1;
+      d.push(names[i] + `(${obj[names[i]]})`);
+    } else {
+      if (d.includes(names[i])) {
+        d.push(names[i] + `(1)`);
+      } else {
+        d.push(names[i]);
+      }
+      obj[names[i]] = 0;
+    }
+  }
+  return d;
 }
+
+
+// function renameFiles(names) {
+//   const obj = {};
+//   const d = [];
+//   for (let i = 0; i < names.length; i++) {
+//     if (names[i] in obj) {
+//       obj[names[i]] += 1;
+//       d.push(a[i] + `(${obj[names[i]]})`);
+//     } else {
+//       if (d.includes(names[i])) {
+//         d.push(names[i] + `(1)`);
+//       } else {
+//         d.push(names[i]);
+//       }
+//       obj[names[i]] = 0;
+//     }
+//   }
+//   return d;
+// }
+
+// let a = ["file", "file", "image", "file(1)", "file"];
+// console.log(renameFiles(a));
+// let obj = {};
+// let d = [];
+// for (let i = 0; i < a.length; i++) {
+//   if (a[i] in obj) {
+//     console.log(obj[a[i]])
+//     obj[a[i]] += 1;
+//     d.push(a[i]+`(${obj[a[i]]})`)
+//   } else {
+//     if (d.includes(a[i])) {
+//       d.push(a[i]+`(1)`)
+//     } else {
+//       d.push(a[i]);
+//     }
+//     obj[a[i]] = 0;
+//   }
+// }
+
+// console.log(obj)
+// console.log(d)
