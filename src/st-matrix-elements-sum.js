c052d6a917132eac064cc958ad9c7a4102415c73
viva <vivasaravian@gmail.com>
2021-09-25 17:02:27 +0300

    complete st-matrix-elements-sum

--- src/st-matrix-elements-sum.js
+++ src/st-matrix-elements-sum.js
@@ -16,7 +16,51 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be 9
  */
-export default function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getMatrixElementsSum(matrix) {
+  const num = [];
+  let n = 0;
+  let matrixLength = matrix[0].length;
+  matrix.push(new Array(matrixLength).fill(0));
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i + 1] === undefined) {
+        break;
+      }
+      if (matrix[i][j] == 0 && matrix[i + 1][j]) {
+        n += matrix[i + 1][j];
+      } else {
+        num.push(matrix[i][j]);
+      }
+    }
+  }
+  return num.reduce((i, j) => i + j, 0) - n;
 }
+
+
+// function getMatrixElementsSum(matrix) {
+//   const num = [];
+//   let n = 0;
+//   let matrixLength = matrix[0].length;
+//   matrix.push(new Array(matrixLength).fill(0));
+//   for (let i = 0; i < matrix.length; i++) {
+//     for (let j = 0; j < matrix[i].length; j++) {
+//       if (matrix[i + 1] === undefined) {
+//         break;
+//       }
+//       if (matrix[i][j] == 0 && matrix[i + 1][j]) {
+//         n += matrix[i + 1][j];
+//       } else {
+//         num.push(matrix[i][j]);
+//       }
+//     }
+//   }
+//   return num.reduce((i, j) => i + j, 0) - n;
+// }
+
+// console.log(
+//   getMatrixElementsSum([
+//     [1, 1, 1],
+//     [2, 2, 2],
+//     [3, 3, 3],
+//   ])
+// );
