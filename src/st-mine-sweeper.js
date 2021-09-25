36d7d882e92dfaaa7d8d2d767541b2387c510832
viva <vivasaravian@gmail.com>
2021-09-25 17:02:27 +0300

    feat: complete st-matrix-elements-sum

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,52 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper(matrix) {
+  let count = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    count.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      count[i][j] = 0;
+      if (matrix[i][j - 1] === true) {
+        count[i][j] += 1;
+      }
+      if (matrix[i][j + 1] !== undefined) {
+        if (matrix[i][j + 1] === true) {
+          count[i][j] += 1;
+        }
+      }
+      if (matrix[i - 1] !== undefined) {
+        if (matrix[i - 1][j] === true) {
+          count[i][j] += 1;
+        }
+      }
+      if (matrix[i + 1] !== undefined) {
+        if (matrix[i + 1][j] === true) {
+          count[i][j] += 1;
+        }
+      }
+      if (matrix[i + 1] !== undefined) {
+        if (matrix[i + 1][j + 1] === true) {
+          count[i][j] += 1;
+        }
+      }
+      if (matrix[i + 1] !== undefined) {
+        if (matrix[i + 1][j - 1] === true) {
+          count[i][j] += 1;
+        }
+      }
+      if (matrix[i - 1] !== undefined) {
+        if (matrix[i - 1][j - 1] === true) {
+          count[i][j] += 1;
+        }
+      }
+      if (matrix[i - 1] !== undefined) {
+        if (matrix[i - 1][j + 1] === true) {
+          count[i][j] += 1;
+        }
+      }
+    }
+  }
+  return count;
 }
