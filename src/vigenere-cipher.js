70f14f4e7dbf0bdb6df8df2f0d78d56b0e554271
viva <vivasaravian@gmail.com>
2021-09-25 18:25:48 +0300

    feat: complete vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,87 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(text, key) {
+    if (!text || !key) {
+      throw Error("Incorrect arguments!");
+    }
+    let encrypted = "";
+    let j = 0;
+    for (let i = 0; i < text.length; i++) {
+      let currentLetter = text[i];
+      const A = 65;
+      const a = 97;
+
+      if (currentLetter.charCodeAt() > 64 && currentLetter.charCodeAt() < 91) {
+        let Pi = currentLetter.charCodeAt(0) - A;
+        let Ki = key[j % key.length].toUpperCase().charCodeAt() - A;
+        let upperLetter = (Pi + (Ki % 26) + 26) % 26;
+
+        encrypted += String.fromCharCode(upperLetter + A);
+
+        j++;
+      } else if (
+        currentLetter.charCodeAt() > 96 &&
+        currentLetter.charCodeAt() < 123
+      ) {
+        let Pi = currentLetter.charCodeAt() - a;
+        let Ki = key[j % key.length].toLowerCase().charCodeAt() - a;
+        let lowerLetter = (Pi + (Ki % 26) + 26) % 26;
+
+        encrypted += String.fromCharCode(lowerLetter + a);
+
+        j++;
+      } else {
+        encrypted += currentLetter;
+      }
+    }
+    if (this.direct !== false) {
+      return encrypted.toUpperCase();
+    } else {
+      return encrypted.toUpperCase().split("").reverse().join("");
+    }
+  }
+  decrypt(encrypted, key) {
+    if (!encrypted || !key) {
+      throw Error("Incorrect arguments!");
+    }
+    let decrypted = "";
+    let j = 0;
+    for (let i = 0; i < encrypted.length; i++) {
+      let currentLetter = encrypted[i];
+      const A = 65;
+      const a = 97;
+
+      if (currentLetter.charCodeAt() > 64 && currentLetter.charCodeAt() < 91) {
+        let Ci = currentLetter.charCodeAt(0) - A;
+        let Ki = key[j % key.length].toUpperCase().charCodeAt() - A;
+        let upperLetter = (Ci - Ki + 26) % 26;
+
+        decrypted += String.fromCharCode(upperLetter + A);
+
+        j++;
+      } else if (
+        currentLetter.charCodeAt() > 96 &&
+        currentLetter.charCodeAt() < 123
+      ) {
+        let Ci = currentLetter.charCodeAt(0) - a;
+        let Ki = key[j % key.length].toLowerCase().charCodeAt() - a;
+        let lowerLetter = (Ci - Ki + 26) % 26;
+
+        decrypted += String.fromCharCode(lowerLetter + a);
+
+        j++;
+      } else {
+        decrypted += currentLetter;
+      }
+    }
+    if (this.direct !== false) {
+      return decrypted.toUpperCase();
+    } else {
+      return decrypted.toUpperCase().split("").reverse().join("");
+    }
   }
 }
-
